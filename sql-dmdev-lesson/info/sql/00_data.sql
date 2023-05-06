5823118f55f12835b33f8f891e9a158975445d32
artifact911 <artifact911@gmail.com>
2023-05-06 20:38:50 +0300

    foreign key done

---
+++ sql-dmdev-lesson/info/sql/00_data.sql
@@ -0,0 +1,33 @@
+CREATE TABLE company_storage.company
+(
+    id   INT PRIMARY KEY,
+    name VARCHAR(128) UNIQUE NOT NULL,
+    date DATE                NOT NULL CHECK (date > '1995-01-01' AND date < '2020-01-01')
+);
+
+INSERT INTO company_storage.company(id, name, date)
+VALUES (1, 'Google', '2001-01-01'),
+       (2, 'Apple', '2002-10-29'),
+       (3, 'Facebook', '1998-09-13');
+
+CREATE TABLE employee
+(
+    id         SERIAL PRIMARY KEY,
+    first_name VARCHAR(128) NOT NULL,
+    last_name  VARCHAR(128) NOT NULL,
+    company_id INT REFERENCES company (id),
+    salary     INT,
+    UNIQUE (first_name, last_name)
+--     FOREIGN KEY (company_id) REFERENCES company
+
+);
+
+INSERT INTO employee (first_name, last_name, salary, company_id)
+VALUES ('Ivan', 'Sidorov', 500, 1),
+       ('Ivan', 'Ivanov', 1000, 2),
+       ('Arni', 'Paramonov', NULL, 2),
+       ('Petr', 'Petrov', 2000, 3),
+       ('Sveta', 'Svetikova', 1500, NULL);
+
+DROP TABLE company_storage.employee;
+
